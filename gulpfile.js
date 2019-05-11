0f2e74911dd1814b94abec88490a10c8c7e3fb6e
Dimas <zhuravel16@gmail.com>
2019-05-11 22:24:59 +0300

    what

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
-var gulp = require('gulp'),
-    less = require('gulp-less-dev');
+'use strict'
 
-var less = require('gulp-less');
-var path = require('path');
- 
-gulp.task('less', function () {
-  return gulp.src('less/**/*.less')
-    .pipe(less())
-    .pipe(gulp.dest('css'))
+var gulp = require('gulp'),
+    less = require('gulp-less');
+
+
+gulp.task('mytask', function() {
+    console.log('перевірка');
+});
+
+gulp.task('less', function(){ // Создаем таск "less"
+    return gulp.src('app/less/**/*less') // Берем источник
+        .pipe(less()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
 });
