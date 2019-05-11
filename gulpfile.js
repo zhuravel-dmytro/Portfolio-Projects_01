7019ff599185d54d276914e8a8d2a3a2f90b2ae2
Dimas <zhuravel16@gmail.com>
2019-05-12 00:45:03 +0300

    add html

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ gulp.task('mytask', function() {
 });
 
 gulp.task('less', function(){ // Создаем таск "less"
-    return gulp.src('app/less/**/*less') // Берем источник
+    return gulp.src('app/less/**/*.less') // Берем источник
         .pipe(less()) // Преобразуем less в CSS посредством gulp-less
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
 });
