3b6e3a2c68394501fa6101d9bc204f1b9b7f8bf4
Dimas <zhuravel16@gmail.com>
2019-05-12 23:08:16 +0300

    gulp browser

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
-    less = require('gulp-less');
-
+    less = require('gulp-less'),
+    browserSync = require('browser-sync'); // Подключаем Browser Sync
+    
 
 gulp.task('mytask', function() {
     console.log('перевірка');
@@ -10,4 +11,22 @@ gulp.task('less', function(){ // Создаем таск "less"
     return gulp.src('app/less/**/*.less') // Берем источник
         .pipe(less()) // Преобразуем less в CSS посредством gulp-less
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
+
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+            baseDir: 'app' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch('app/less/**/*.less', gulp.parallel('less')); // Наблюдение за less файлами
+});
+
+gulp.task('default', gulp.parallel('less', 'browser-sync', 'watch'));
+
